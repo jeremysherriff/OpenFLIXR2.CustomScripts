88793528a7960a4d28d86e8d07388c60202c3667
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-01-07 21:33:23 +1300

    Updates to logging

--- stop_unwanted_apps.sh
+++ stop_unwanted_apps.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-exec 1> >(tee -a /var/log/userscript.log) 2>&1
+exec 1> >(tee -a /var/log/stop_unwanted_apps.log) 2>&1
 TODAY=$(date)
 echo "-----------------------------------------------------"
 echo "Date:          $TODAY"
 echo "-----------------------------------------------------"
 
-# Remove OpenFlixr's normal Lets Encrypt update
-if [ -f /etc/cron.weekly/letsrenew.sh ]; then
-  rm /etc/cron.weekly/letsrenew.sh
-fi
-
 # Stop all the things that don't normally auto-start, as @MediaJunkie is a bit aggressive
 # starting everything whether we want it or not.
-echo ""
 EXTRAS="monit sickchill lazylibrarian syncthing@openflixr couchpotato headphones autosub lidarr mylar home-assistant nzbget qbittorrent ubooquity"
 for app in $EXTRAS; do
   echo -ne "Checking $app.. "
@@ -27,14 +21,4 @@ for app in $EXTRAS; do
   fi
   echo ""
 done
-
-# Self-installed package updates
-echo "SSL Cert Check:"
-cd /opt/ssl-cert-check
-git reset --hard
-git pull
-echo ""
- 
-chmod +x /home/openflixr/scripts/module-updates/*
-run-parts -v /home/openflixr/scripts/module-updates
 
