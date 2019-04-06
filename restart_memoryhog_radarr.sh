0818e683d8734118368f507e7867ff430c72d337
root <root@openflixr.com>
2019-04-06 18:05:48 +1300

    Restart script for Mono/Radarr memory leak

---
+++ restart_memoryhog_radarr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "$1" == "-v" || "$1" == "--verbose" ]]; then
+    MYDEBUG=1
+fi
+
+CEILING=2400000
+RAM=$(ps -aux | grep -i radarr | grep -v grep | grep -v memoryhog | awk '{print $5}')
+
+if [[ -n $MYDEBUG ]]; then echo "RAM usage is at $RAM bytes"; fi
+
+if [[ -n $RAM ]]; then
+    if [[ $RAM -gt $CEILING ]]; then
+        if [[ -n $MYDEBUG ]]; then echo "RAM usage is above ceiling, restarting service"; fi
+        systemctl restart radarr
+    else
+        if [[ -n $MYDEBUG ]]; then echo "RAM usage is fine"; fi
+    fi
+else
+    if [[ -n $MYDEBUG ]]; then echo "Could not compute RAM usage, is service running?"; fi
+fi
+
