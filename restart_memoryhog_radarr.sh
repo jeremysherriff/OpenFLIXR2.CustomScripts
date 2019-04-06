43811943bdfe9fc200ae509a983a85f33920faa3
root <root@openflixr.com>
2019-04-06 18:07:58 +1300

    Adjust memory ceiling for Radarr Mono mem leak

--- restart_memoryhog_radarr.sh
+++ restart_memoryhog_radarr.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "-v" || "$1" == "--verbose" ]]; then
     MYDEBUG=1
 fi
 
-CEILING=2400000
+CEILING=2600000
 RAM=$(ps -aux | grep -i radarr | grep -v grep | grep -v memoryhog | awk '{print $5}')
 
 if [[ -n $MYDEBUG ]]; then echo "RAM usage is at $RAM bytes"; fi
