ccdf06431ec1386512a43a39db1aa04dfbffc2af
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-04-26 18:03:10 +1200

    Log output to file

--- restart_memoryhog_radarr.sh
+++ restart_memoryhog_radarr.sh
@@ -13,16 +13,16 @@ function info () {
 
 RAM=$(ps -aux | grep -i radarr | grep -v grep | grep -v memoryhog | awk '{print $5}')
 
-if [[ -n $MYDEBUG ]]; then echo "RAM usage is at $RAM bytes"; fi
+if [[ -n $MYDEBUG ]]; then info "RAM usage is at $RAM bytes"; fi
 
 if [[ -n $RAM ]]; then
     if [[ $RAM -gt $CEILING ]]; then
-        if [[ -n $MYDEBUG ]]; then echo "RAM usage is above ceiling, restarting service"; fi
+        if [[ -n $MYDEBUG ]]; then info "RAM usage is above ceiling, restarting service"; fi
         systemctl try-reload-or-restart radarr
     else
-        if [[ -n $MYDEBUG ]]; then echo "RAM usage is fine"; fi
+        if [[ -n $MYDEBUG ]]; then info "RAM usage is fine"; fi
     fi
 else
-    if [[ -n $MYDEBUG ]]; then echo "Could not compute RAM usage, is service running?"; fi
+    if [[ -n $MYDEBUG ]]; then info "Could not compute RAM usage, is service running?"; fi
 fi
 
--- restart_memoryhog_sonarr.sh
+++ restart_memoryhog_sonarr.sh
@@ -14,16 +14,16 @@ function info () {
 
 RAM=$(ps -aux | grep -i nzbdrone | grep -v grep | grep -v memoryhog | awk '{print $5}')
 
-if [[ -n $MYDEBUG ]]; then echo "RAM usage is at $RAM bytes"; fi
+if [[ -n $MYDEBUG ]]; then info "RAM usage is at $RAM bytes"; fi
 
 if [[ -n $RAM ]]; then
     if [[ $RAM -gt $CEILING ]]; then
-        if [[ -n $MYDEBUG ]]; then echo "RAM usage is above ceiling, restarting service"; fi
+        if [[ -n $MYDEBUG ]]; then info "RAM usage is above ceiling, restarting service"; fi
         systemctl try-reload-or-restart sonarr
     else
-        if [[ -n $MYDEBUG ]]; then echo "RAM usage is fine"; fi
+        if [[ -n $MYDEBUG ]]; then info "RAM usage is fine"; fi
     fi
 else
-    if [[ -n $MYDEBUG ]]; then echo "Could not compute RAM usage, is service running?"; fi
+    if [[ -n $MYDEBUG ]]; then info "Could not compute RAM usage, is service running?"; fi
 fi
 
