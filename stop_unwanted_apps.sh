293df75d4f0723aded7200adb6d41e4ae6df0b06
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-05-27 15:57:57 +1200

    Use Monit for service management

--- stop_unwanted_apps.sh
+++ stop_unwanted_apps.sh
@@ -7,17 +7,14 @@ echo "-----------------------------------------------------"
 
 # Stop all the things that don't normally auto-start, as @MediaJunkie is a bit aggressive
 # starting everything whether we want it or not.
-EXTRAS="monit sickchill lazylibrarian syncthing@openflixr couchpotato headphones autosub lidarr mylar home-assistant nzbget qbittorrent ubooquity"
+EXTRAS="sickchill lazylibrarian syncthing couchpotato headphones autosub lidarr mylar homeassistant nzbget qbittorrent ubooquity nzbhydra sabnzbd mopidy"
+# EXTRAS="monit sickchill lazylibrarian syncthing@openflixr couchpotato headphones autosub lidarr mylar home-assistant nzbget qbittorrent ubooquity"
 for app in $EXTRAS; do
-  echo -ne "Checking $app.. "
-  systemctl is-enabled $app 2>/dev/null
+  echo -e "Checking $app.. "
+  monit status $app | grep "monitoring status" | grep "Not monitored"
   if [ $? -ne 0 ]; then
-    echo -ne " and is currently.. "
-    systemctl is-active $app 2>/dev/null
-    if [ $? -eq 0 ]; then
       echo " Stopping $app"
-      systemctl stop $app
-    fi
+      monit stop $app
   fi
   echo ""
 done
