3b715b8c9a462b900ce237d9133d18fbfe818895
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-05-31 22:49:39 +1200

    Cleanup to remove old code

--- stop_unwanted_apps.sh
+++ stop_unwanted_apps.sh
@@ -98,19 +98,3 @@ for s in *; do
     esac
 done
 
-exit
-
-# Stop all the things that don't normally auto-start, as @MediaJunkie is a bit aggressive
-# starting everything whether we want it or not.
-EXTRAS="sickchill lazylibrarian syncthing couchpotato headphones autosub lidarr mylar homeassistant nzbget qbittorrent ubooquity nzbhydra sabnzbd mopidy"
-# EXTRAS="monit sickchill lazylibrarian syncthing@openflixr couchpotato headphones autosub lidarr mylar home-assistant nzbget qbittorrent ubooquity"
-for app in $EXTRAS; do
-  echo -e "Checking $app.. "
-  monit status $app | grep "monitoring status" | grep "Not monitored"
-  if [ $? -ne 0 ]; then
-      echo " Stopping $app"
-      monit stop $app
-  fi
-  echo ""
-done
-
