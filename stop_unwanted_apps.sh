ae3ba3552f5edceaac69c6a9fb4451b19c6523fc
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-05-31 22:38:39 +1200

    Use ini file to define service actions

--- stop_unwanted_apps.sh
+++ stop_unwanted_apps.sh
@@ -1,9 +1,104 @@
 #!/bin/bash
+CONF_BASE=/etc/monit/conf.d
+
+if [[ "$1" == "--help" || "$1" == "-?" || "$1" == "-h" ]]; then
+    echo ""
+    echo "  stop_unwanted_apps.sh [OPTION]"
+    echo ""
+    echo "  Automates service actions based on entries in /opt/custom/stop_unwanted_apps.ini"
+    echo "  Services are those defined in Monit service files in $CONF_BASE"
+    echo "      -?     : This help"
+    echo "      -l     : List out all the Monit services detected on this system"
+    echo "      -q     : Quiet"
+    echo ""
+    echo "  Only one option can be specified due to my poor coding skills :)"
+    echo ""
+    exit 1
+fi
+
+if [[ ! -d $CONF_BASE ]]; then
+    echo ""
+    echo "ERROR: Monit config files not found in expected location: $CONF_BASE"
+    echo ""
+    exit 1
+fi
+
+if [[ "$1" == "-l" ]]; then
+    echo ""
+    echo "Listing Monit service definitions in $CONF_BASE:"
+    cd $CONF_BASE
+    for s in *; do
+        echo "  $s"
+    done
+    echo ""
+    exit 0
+fi
+
+if [[ "$1" == "-q" ]]; then
+    VERBOSE=0
+else
+    VERBOSE=1
+fi
+
+debug () {
+  if [[ $VERBOSE == 1 ]]; then
+    echo "$*"
+  fi
+}
+
 exec 1> >(tee -a /var/log/stop_unwanted_apps.log) 2>&1
 TODAY=$(date)
-echo "-----------------------------------------------------"
-echo "Date:          $TODAY"
-echo "-----------------------------------------------------"
+debug "-----------------------------------------------------"
+debug "Date:          $TODAY"
+debug "-----------------------------------------------------"
+
+cd $CONF_BASE
+for s in *; do
+    debug "Evaluating Monit service $s:"
+    ACTION=$(crudini --get /opt/custom/stop_unwanted_apps.ini $s action 2>/dev/null || echo "notdefined")
+    debug "  Requested action is $ACTION"
+
+    case $ACTION in 
+      start)
+        debug "    Actioning 'monit start' on $s"
+        monit start $s
+        debug ""
+      ;;
+      stop)
+        debug "    Actioning 'monit stop' on $s"
+        monit stop $s
+        debug ""
+      ;;
+      autostart)
+        debug "    Actioning 'monit start' on $s"
+        monit start $s
+        SERVICENAME=$(crudini --get /opt/custom/stop_unwanted_apps.ini $s servicename 2>/dev/null || echo "$s")
+        if [[ "$SERVICENAME" != "$s" ]]; then
+            debug "  Overriding service name as $SERVICENAME"
+        fi
+        debug "    Actioning 'systemctl enable' on $SERVICENAME"
+        systemctl enable $SERVICENAME 2>/dev/null
+        debug ""
+      ;;
+      disable)
+        debug "    Actioning 'monit stop' on $s"
+        monit stop $s
+        SERVICENAME=$(crudini --get /opt/custom/stop_unwanted_apps.ini $s servicename 2>/dev/null || echo "$s")
+        if [[ "$SERVICENAME" != "$s" ]]; then
+            debug "  Overriding service name as $SERVICENAME"
+        fi
+        debug "    Actioning 'systemctl disable' on $SERVICENAME"
+        systemctl disable $SERVICENAME 2>/dev/null
+        debug ""
+      ;;
+      *)
+        debug "    Ignoring definition $s"
+        debug ""
+      ;;
+    esac
+done
+
+exit
 
 # Stop all the things that don't normally auto-start, as @MediaJunkie is a bit aggressive
 # starting everything whether we want it or not.
