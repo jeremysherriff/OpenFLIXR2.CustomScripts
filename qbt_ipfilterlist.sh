9c142b1281159bb33ec631590011899b8f062db2
Jeremy Sherriff <jeremysherriff@gmail.com>
2019-12-14 15:49:31 +1100

    API v2. Added support for nox service

--- qbt_ipfilterlist.sh
+++ qbt_ipfilterlist.sh
@@ -20,14 +20,12 @@ http://list.iblocklist.com/?list=bt_bogon
 
 wget "${URLS[@]}" -O - | gunzip | LC_ALL=C sort -u >"/home/mediabox/.config/qBittorrent/ipfilter.p2p"
 chown --reference=/home/mediabox/.config/qBittorrent/ /home/mediabox/.config/qBittorrent/ipfilter.p2p
-ISACTIVE=$(systemctl is-active qbittorrent)
-if [[ $ISACTIVE == "active" ]]; then
+if [[ $(systemctl is-active qbittorrent) == "active" || $(systemctl is-active qbittorrent-nox) == "active" ]]; then
 	echo ""
 	echo Reloading qBitTorrent IP filter
-	#systemctl restart qbittorrent
-	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"false\"}" http://127.0.0.1:8080/command/setPreferences
+	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"false\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
 	sleep 1
-	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"true\"}" http://127.0.0.1:8080/command/setPreferences
+	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"true\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
 	sleep 1
 	tail -10 /home/mediabox/.local/share/data/qBittorrent/logs/qbittorrent.log | grep filter
 fi
