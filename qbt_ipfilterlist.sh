98bb4dcfcdba4c36d453355030a5a703a21c7e1d
Jeremy Sherriff <jeremysherriff@gmail.com>
2020-07-07 18:02:26 +1200

    Enhanced logging, exit loop on error instead of exiting script

--- qbt_ipfilterlist.sh
+++ qbt_ipfilterlist.sh
@@ -18,14 +18,12 @@ http://list.iblocklist.com/?list=bt_level3
 http://list.iblocklist.com/?list=bt_bogon
 )
 
-wget "${URLS[@]}" -O - | gunzip | LC_ALL=C sort -u >"/home/mediabox/.config/qBittorrent/ipfilter.p2p"
-chown --reference=/home/mediabox/.config/qBittorrent/ /home/mediabox/.config/qBittorrent/ipfilter.p2p
-if [[ $(systemctl is-active qbittorrent) == "active" || $(systemctl is-active qbittorrent-nox) == "active" ]]; then
+wget "${URLS[@]}" --no-verbose -O - | gunzip | LC_ALL=C sort -u>"/mnt/downloads/ipfilter.p2p"
+# if [[ $(systemctl is-active qbittorrent) == "active" || $(systemctl is-active qbittorrent-nox) == "active" ]]; then
 	echo ""
 	echo Reloading qBitTorrent IP filter
 	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"false\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
 	sleep 1
 	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"true\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
-	sleep 1
-	tail -10 /home/mediabox/.local/share/data/qBittorrent/logs/qbittorrent.log | grep filter
-fi
+	 tail /opt/docker/qbittorrent/config/data/qBittorrent/logs/qbittorrent.log | grep filter
+# fi
