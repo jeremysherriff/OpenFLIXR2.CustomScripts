e73e9f361d27ae238fac433ec99213917bd50c1b
Jeremy Sherriff <jeremysherriff@gmail.com>
2021-03-23 20:00:10 +1300

    Use email.ini for mailer settings

--- daily_reboot.sh
+++ daily_reboot.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-if [[ -f /var/run/reboot-required ]]; then
-	/usr/bin/sendemail -f tvbox@bgs.co.nz -t alerts@bgs.co.nz -s 192.168.220.9:25 -o tls=no -u "TVBox reboot" -o message-file=/var/run/reboot-required.pkgs -o message-content-type=text -q
-	/sbin/shutdown -r +5 2>&1 | logger -t reboot
+if [[ ! -f /opt/custom/notify/email.ini ]]; then
+        echo ""
+        echo "Mail settings not defined!"
+        echo "Please review and edit /opt/custom/notify/email.ini"
+        cp /opt/custom/notify/.email.ini.dist /opt/custom/notify/email.ini
+        echo ""
+        exit 2
 fi
 
+if [[ ! -f /var/run/reboot-required ]]; then
+	exit 0
+fi
+
+## Externalised email settings
+if [[ $(crudini --get /opt/custom/notify/email.ini Email SMTP_AUTH | awk '{print $1}') == "yes" ]]; then
+        SMTP_USER="-xu $(crudini --get /opt/custom/notify/email.ini Email SMTP_USER)"
+        SMTP_PASS="-xp $(crudini --get /opt/custom/notify/email.ini Email SMTP_PASS)"
+fi
+if [[ -n $(crudini --get /opt/custom/notify/email.ini Email CC_ADDRESS) ]]; then
+        CC_ADDRESS="-cc $(crudini --get /opt/custom/notify/email.ini Email CC_ADDRESS)"
+fi
+
+FROM_ADDRESS=$(crudini --get /opt/custom/notify/email.ini Email FROM_ADDRESS)
+TO_ADDRESS=$(crudini --get /opt/custom/notify/email.ini Email TO_ADDRESS)
+USE_TLS=$(crudini --get /opt/custom/notify/email.ini Email USE_TLS | awk '{print $1}')
+MAIL_SERVER=$(crudini --get /opt/custom/notify/email.ini Email MAIL_SERVER)
+MAIL_PORT=$(crudini --get /opt/custom/notify/email.ini Email MAIL_PORT)
+if [[ -n $DISPLAY_NAME ]]; then
+        FROM_ADDRESS="$DISPLAY_NAME<$FROM_ADDRESS>"
+fi
+
+/usr/bin/sendemail -f $FROM_ADDRESS -t $TO_ADDRESS $CC_ADDRESS -u "`hostname` reboot" -o tls=$USE_TLS -s "$MAIL_SERVER:$MAIL_PORT" $SMTP_USER $SMTP_PASS -o message-file=/var/run/reboot-required.pkgs -o message-content-type=text -q
+
+/sbin/shutdown -r +5 2>&1 | logger -t reboot
+exit 1
