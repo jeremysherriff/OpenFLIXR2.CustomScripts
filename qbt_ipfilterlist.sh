57c19541f7eabd07e98bd6f76f67ab4cc5b59429
Jeremy Sherriff <jeremysherriff@gmail.com>
2021-08-11 20:07:27 +1200

    Update api to use reverse proxy. Add extra blocklist. Use newer blocklist url format.

--- qbt_ipfilterlist.sh
+++ qbt_ipfilterlist.sh
@@ -11,20 +11,26 @@ echo "Date:          $TODAY"
 
 set -e
 
+# http://list.iblocklist.com/?list=bt_level1
+# http://list.iblocklist.com/?list=bt_level2
+# http://list.iblocklist.com/?list=bt_level3
+# http://list.iblocklist.com/?list=bt_bogon
 URLS=(
-http://list.iblocklist.com/?list=bt_level1
-http://list.iblocklist.com/?list=bt_level2
-http://list.iblocklist.com/?list=bt_level3
-http://list.iblocklist.com/?list=bt_bogon
+http://list.iblocklist.com/?list=cwworuawihqvocglcoss\&fileformat=p2p\&archiveformat=gz
+http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw\&fileformat=p2p\&archiveformat=gz
+http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw\&fileformat=p2p\&archiveformat=gz
+http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh\&fileformat=p2p\&archiveformat=gz
+http://list.iblocklist.com/?list=gihxqmhyunbxhbmgqrla\&fileformat=p2p\&archiveformat=gz
+http://list.iblocklist.com/?list=xpbqleszmajjesnzddhv\&fileformat=p2p\&archiveformat=gz
 )
 
-wget "${URLS[@]}" --no-verbose -O - | gunzip | LC_ALL=C sort -u>"/mnt/downloads/ipfilter.p2p"
-# if [[ $(systemctl is-active qbittorrent) == "active" || $(systemctl is-active qbittorrent-nox) == "active" ]]; then
-	echo ""
-	echo Reloading qBitTorrent IP filter
-	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"false\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
-	sleep 2
-	/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"true\"}" http://127.0.0.1:8080/api/v2/app/setPreferences
-	sleep 2
-	 tail /opt/docker/qbittorrent/config/data/qBittorrent/logs/qbittorrent.log | grep filter
-# fi
+wget "${URLS[@]}" --no-verbose -O - | gunzip | LC_ALL=C sort -u | grep -E . > /mnt/downloads/ipfilter.new
+mv /mnt/downloads/ipfilter.p2p /mnt/downloads/ipfilter.p2p.old
+mv /mnt/downloads/ipfilter.new /mnt/downloads/ipfilter.p2p
+
+echo ""
+echo Reloading qBitTorrent IP filter
+/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"false\"}" http://127.0.0.1/qbt/api/v2/app/setPreferences
+sleep 2
+/usr/bin/wget -q -O - --header="Content-type:application/x-www-form-urlencoded" --post-data="json={\"ip_filter_enabled\":\"true\"}" http://127.0.0.1/qbt/api/v2/app/setPreferences
+# tail /opt/docker/qbittorrent/config/data/qBittorrent/logs/qbittorrent.log | grep filter
