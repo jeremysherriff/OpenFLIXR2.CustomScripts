95e8b7c3396e19acd2b487adbd8d31f96e8573b2
Jeremy Sherriff <jeremysherriff@gmail.com>
2024-07-30 19:44:21 +1200

    Add --category filtering

--- clear_qbt_complete.py
+++ clear_qbt_complete.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 
 parser=argparse.ArgumentParser()
+parser.add_argument('-c','--category', help='Torrent category to clear.\n If not specified, all torrents are cleared.\n Use \"nocategory\" for torrents with no category assigned.')
 parser.add_argument('-b','--baseurl', help='Override url to qBitTorrent. Defaults to http://localhost:8080/')
 parser.add_argument('-v','--verbose', action="store_true", help='Verbose output for debug')
 parser.add_argument('-d','--debug', action="store_true", help='Do not action. Implies -v')
@@ -22,6 +23,7 @@ rootLogger.setLevel(logging.INFO)
 
 debugmode = False
 testmode = False
+category = 'everything'
 baseurl = 'http://localhost:8080/'
 
 consoleHandler = logging.StreamHandler(sys.stdout)
@@ -34,10 +36,16 @@ if args.verbose or args.debug:
     rootLogger.setLevel(logging.DEBUG)
 if args.debug:
     testmode = True
+if args.category:
+    category = args.category
 if args.baseurl:
     baseurl = args.baseurl
 
+logging.debug('category is '+category)
 logging.debug('baseurl is '+baseurl)
+
+if category == 'nocategory' :
+    category = ''
 
 # OK lets do stuff
 exitcode = 0
@@ -59,14 +67,28 @@ try:
         exit(0)
     for tor in data:
         if tor['state'] != 'pausedUP':
-            logging.debug('Skipping '+tor['state']+' torrent: '+tor['name'])
+            logging.debug('Skipping torrent with state '+tor['state']+': '+tor['name'])
             continue
-        if tor['category'] == 'radarr' or tor['category'] == 'sonarr' :
+
+        if tor['category'] == '' :
+            cat_name = '(empty)'
+        else :
+            cat_name = tor['category']
+
+        if category != 'everything' and tor['category'] != category :
+            logging.debug('Skipping torrent with category '+cat_name+': '+tor['name'])
+            continue
+
+        # Torrents handled via the *arrs can have data deleted, otherwise preserve torrent content
+        if tor['category'][-3:] == "arr" :
             form_data = { 'hashes': str(tor['hash']), 'deleteFiles':'true' }
+            logging.debug('Planning to clear torrent and data: '+tor['name'])
         else :
             form_data = { 'hashes': str(tor['hash']), 'deleteFiles':'false' }
+            logging.debug('Planning to clear torrent but leave data: '+tor['name'])
+
         url = 'api/v2/torrents/delete'
-        logging.info('Clearing '+tor['state']+' torrent: '+tor['name'])
+        logging.info('Clearing '+tor['state']+' torrent with category '+cat_name+': '+tor['name'])
         if not testmode:
             try:
                 response = requests.post(baseurl+url,data=form_data)
